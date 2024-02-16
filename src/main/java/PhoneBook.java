b4dfa708abd8948c4996bb9dbcfbac5adc6889b9
Viktor <v.kurochkin89@yandex.ru>
2024-02-16 15:02:21 +0300

    Добавил словарь для имен и номеров

---
+++ src/main/java/PhoneBook.java
@@ -0,0 +1,8 @@
+import java.util.HashMap;
+import java.util.Set;
+
+public class PhoneBook {
+    // словарь для имен и номеров
+    private HashMap<String, Set<String>> phoneBook = new HashMap<>();
+
+}
